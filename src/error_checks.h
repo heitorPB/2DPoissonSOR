2d51ad66288e062774e738eb369f4a7ef0b84d58
Heitor P. de Bittencourt <heitorPB@users.noreply.github.com>
2016-01-03 17:54:26 +0200

    Improved cuda error_checks for more info and doc

--- src/error_checks.h
+++ src/error_checks.h
@@ -2,6 +2,7 @@
  * @file
  *
  * This header provides two helper macros for error checking.
+ *
  * File modified from CUDA samples/common/inc/helper_cuda.h
  */
 
@@ -13,10 +14,35 @@
 #include <stdlib.h>
 
 
+/** @brief wrap CUDA API calls with this macro to check for errors.
+ *
+ * Usage example:
+ *
+ * 	CUDA_CHECK(cudaMemcpy((void*) f, (void*) f_gpu, size, cudaMemcpyDeviceToHost));
+ *
+ * Example of output to stderr:
+ *
+ * 	CUDA error at PoissonSOR2D_CUDA.c:61 code=77 "cudaMemcpy((void*) f, (void*) f_gpu, size, cudaMemcpyDeviceToHost)" 
+ */
 #define CUDA_CHECK(errarg)   __checkErrorFunc((errarg), #errarg, __FILE__, __LINE__)
+
+
+/** @brief wrapper for cudaGetLastError() with a custom message
+ *
+ * Usage example:
+ *
+ * 	update_CUDA<<<grid, block>>>(NULL, f_tmp, gamma, N);
+ * 	cudaThreadSynchronize();
+ * 	CHECK_ERROR_MSG("Kernel call");
+ *
+ * Example of output to stderr:
+ *
+ * 	PoissonSOR2D_CUDA.c(61) : cudaGetLastError() : Kernel call : (77) an illegal memory access was encountered.
+ */
 #define CHECK_ERROR_MSG(errstr) __checkErrMsgFunc(errstr, __FILE__, __LINE__)
 
 
+/** @cond */
 inline void __checkErrorFunc(cudaError_t errarg,
                              char const *const func,
                              const char* file,
@@ -36,10 +62,13 @@ inline void __checkErrMsgFunc(const char* errstr,
 {
 	cudaError_t err = cudaGetLastError();
 	if(err != cudaSuccess) {
-		fprintf(stderr, "Error: %s at %s(%i): %s\n",
-				errstr, file, line, cudaGetErrorString(err));
+		fprintf(stderr,
+		        "%s(%i) : cudaGetLastError() : %s : (%d) %s.\n",
+		        file, line, errstr, (int)err, cudaGetErrorString(err));
 		exit(EXIT_FAILURE);
 	}
 }
+
+/** @endcond */
 
 #endif
